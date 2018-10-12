356869ed134cd5ab5fd6acb04268305a484c422e
Pau <pauiglesias@gmail.com>
2018-10-13 01:08:32 +0200

    Front context detection.

--- core/core.php
+++ core/core.php
@@ -21,11 +21,46 @@ final class Core extends Helpers\Singleton {
 	 */
 	protected function onConstruct() {
 
-		// Factory object
-		$this->plugin->factory = new Factory($this->plugin);
+		// Check context
+		if ($this->front()) {
 
-		// WP loaded hook
-		add_action('wp_loaded', [$this, 'loaded'], PHP_INT_MAX);
+			// Factory object
+			$this->plugin->factory = new Factory($this->plugin);
+
+			// WP loaded hook
+			add_action('wp_loaded', [$this, 'loaded'], PHP_INT_MAX);
+		}
+	}
+
+
+
+	/**
+	 * Check front context
+	 */
+	private function front() {
+
+		// Admin
+		if (is_admin()) {
+			return false;
+		}
+
+		// Avoid CRON requests
+		if (defined('DOING_CRON') && DOING_CRON) {
+			return false;
+		}
+
+		// XML-RPC request
+		if (defined('XMLRPC_REQUEST') && XMLRPC_REQUEST) {
+			return false;
+		}
+
+		// No WP-Cli allowed
+		if (defined('WP_CLI') && WP_CLI) {
+			return false;
+		}
+
+		// Allow
+		return true;
 	}
 
 
