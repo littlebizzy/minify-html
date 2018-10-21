7d026c527dc0baec9f38b1fa5996ea0de30b0adc
Pau <pauiglesias@gmail.com>
2018-10-21 21:09:37 +0200

    CSS parsing and minification.

--- html/parser.php
+++ html/parser.php
@@ -14,6 +14,21 @@ class Parser {
 
 
 	/**
+	 * Input args
+	 */
+	private $args;
+
+
+
+	/**
+	 * Constants for tag delimitation
+	 */
+	const TAG_INI = '370748f2338fd94c291b227346333735';
+	const TAG_END = '4c79af2f7f3a90cc0c7fed9fd42172d9';
+
+
+
+	/**
 	 * Default args
 	 */
 	private $defaults = [
@@ -26,13 +41,6 @@ class Parser {
 		'conditional'	=> false,
 		'selfClosing'	=> false,
 	];
-
-
-
-	/**
-	 * Input args
-	 */
-	private $args;
 
 
 
@@ -62,23 +70,98 @@ class Parser {
 			// Prepare line feeds
 			$html = str_replace(chr(13).chr(10), chr(10), $html);
 
-			if ($comments) {
+			// Transforms
+			$tags_src = [];
+			$tags_new = [];
+			$tags = ['script', 'pre', 'textarea', 'style'];
 
+			// Prepare delimiters
+			foreach ($tags as $tag) {
+
+				// Tag
+				$ini = '<'.$tag;
+				$end = '/'.$tag.'>';
+
+				// Source and new tag
+				$tags_src[] = $ini;
+				$tags_src[] = $end;
+				$tags_new[] = self::TAG_INI.$ini;
+				$tags_new[] = $end.self::TAG_END;
 			}
 
-			if ($styles) {
+			// Splits the content
+			$parts = str_ireplace($tags_src, $tags_new, $html);
+			$parts = explode(self::TAG_END, $parts);
 
+			// Init
+			$minified = '';
+
+			// Enum parts
+			foreach ($parts as $part) {
+
+				// Find tag start
+				$pos = stripos($part, self::TAG_INI);
+				if (false === $pos) {
+					$before = $part;
+					$inside = '';
+
+				// Full tag
+				} else {
+
+					// Detect before and inside content
+					$before = substr($part, 0, $pos);
+					$inside = substr($part, $pos + 32);
+
+					// Process styles
+					if ('<style' == strtolower(substr($inside, 0, 6))) {
+
+						// Check transformation
+						if ($styles) {
+
+							// Remove tag start and end and spaces
+							$inside = preg_replace(['/\>[^\S ]+/'.$pm, '/[^\S ]+\</'.$pm, '/(\s)+/'.$pm], ['>', '<', '\\1'], $inside);
+
+							// Remove CSS comments
+							if ($comments) {
+								$inside = preg_replace('!/\*[^*]*\*+([^/][^*]*\*+)*/!', '', $inside);
+							}
+
+							// Minify
+							$inside = str_replace([chr(10), ' {', '{ ', ' }', '} ', '( ', ' )', ' :', ': ', ' ;', '; ', ' ,', ', ', ';}'],
+													['', '{', '{', '}', '}', '(', ')', ':', ':', ';', ';', ',', ',', '}'], $inside);
+						}
+
+					// Process scripts
+					} elseif ('<script' == strtolower(substr($inside, 0, 7))) {
+
+						// Check transformation
+						if ($scripts) {
+
+							// Remove Javascript comments
+							if ($comments) {
+
+							}
+						}
+					}
+				}
+
+				// Remove HTML comments
+				if ($comments) {
+
+				}
+
+				// Add chunk
+				$minified .= $before.$inside;
 			}
 
-			if ($scripts) {
-
-			}
+			// Done
+			$html = $minified;
 		}
 
 		/**
 		 * Removes self-closing markup for HTML5 documents
 		 */
-		if ($selfClosing) {
+if (false && $selfClosing) {
 			$test = strtolower(substr(ltrim($html, 0, 15)));
 			if ($test == '<!doctype html>') {
 				$html = str_replace(' />', '>', $buffer);
@@ -90,12 +173,12 @@ class Parser {
 		}
 
 		// Remove extra spacing
-		if ($spacing) {
+if (false && $spacing) {
 			$html = preg_replace('/\s+/', ' ', trim($html));
 		}
 
 		// Remove line breaks
-		if ($lineBreaks) {
+if (false && $lineBreaks) {
 			$html = str_replace(chr(13).chr(10), chr(10), $html);
 			$html = str_replace(chr(10), '', $html);
 		}
